286c4028ce5477a0ad3a6f17fb945bbc256e5c3c
alittleie <a.little.ie@gmail.com>
2021-09-26 11:56:07 -0500

    Updating formatting function to allow entry by format_type and col list. This was included as a new level "level 1"

--- clean.py
+++ clean.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 from time import sleep
 
+
 class Clean:
     df = None
 
@@ -33,10 +34,10 @@ class Clean:
 
             if col_len_0 != col_len_1:
                 col_bool = True
-                print("****** %d columns removed with all NAN *****" % (col_len_0-col_len_1))
+                print("****** %d columns removed with all NAN *****" % (col_len_0 - col_len_1))
 
             # same as previous but no drop any rows containing NAN
-            row_len_0= len(self.df)
+            row_len_0 = len(self.df)
             self.df = self.df.dropna(axis=0, how='any')
             row_len_1 = len(self.df)
 
@@ -49,7 +50,7 @@ class Clean:
         print('--------------------------------------------------------------')
 
     # segment to either auto format columns or go through individually to convert to usable format
-    def auto_format(self, level, auto=True):
+    def format(self, level, format_type=0, col=None, auto=False):
         print('\n')
         print('--------------------------------------------------------------')
 
@@ -62,17 +63,19 @@ class Clean:
                 col_bool = False
                 while col_bool == False:
                     print("Column number %d out of %d" % (l, j), '\n')
-                    l+=1
+                    l += 1
                     # prints out column info
                     print(self.df[col].head, '\n')
 
-                    detail = input("Do you want to see more info about the column values \n enter 0 for yes or 1 no: \n")
+                    detail = input(
+                        "Do you want to see more info about the column values \n enter 0 for yes or 1 no: \n")
                     if detail == '1':
                         pass
                     else:
                         print(self.df[col].unique(), '\n')
 
-                    method = input("Enter:  \n 0 for pass \n 1 for nominal features \n 2 for ordinal \n 3 One Hot \n 4 for target \n 6 for drop: \n ")
+                    method = input(
+                        "Enter:  \n 0 for pass \n 1 for nominal features \n 2 for ordinal \n 3 One Hot \n 4 for target \n 6 for drop: \n ")
 
                     if method == '0':
                         col_bool = True
@@ -118,21 +121,131 @@ class Clean:
                     if method == '4':
                         self.df['Target'] = self.df[col]
                         self.df = self.df.drop(axis=1, columns=[col])
-                        target_change= input('Do you want to change the target further 0 for yes 1 for no: ')
-                        if target_change =='0':
+                        target_change = input('Do you want to change the target further 0 for yes 1 for no: ')
+                        if target_change == '0':
                             col = "Target"
                         else:
                             col_bool = True
                     if method == '6':
-                        self.df =self.df.drop(axis =1, columns= [col])
+                        self.df = self.df.drop(axis=1, columns=[col])
                         print('\n')
                         print('Column Dropped')
                         sleep(1.5)
                         col_bool = True
                 print('\n')
                 print('--------------------------------------------------------------')
-        print('--------------------------------------------------------------')
 
+        # this segment allows you to choose formatting type by col
+        elif level == 1 and auto is False:
+            index_bool = True
+            col_list = self.df.columns
+
+            if col[0] in col_list:
+                index_bool = False
+
+
+            if format_type == 0:
+                pass
+            # nominal feature
+            elif format_type == 1:
+                print('here')
+                le = LabelEncoder()
+                if index_bool is True:
+                    for i in col:
+                        self.df[col_list[i]] = le.fit_transform(self.df[col_list[i]].astype(str))
+                        print(self.df[col_list[i]])
+                        sleep(1.5)
+                elif index_bool is False:
+                    for col in col:
+                        self.df[col] = le.fit_transform(self.df[col].astype(str))
+                        print(self.df[col])
+                        sleep(1.5)
+            # ordinal features
+            elif format_type == 2:
+
+                if index_bool is True:
+                    for i in col:
+                        print(i)
+                        mapping_bool = False
+                        while mapping_bool == False:
+                            map_dic = {}
+                            working = False
+                            while working == False:
+                                map_list = []
+                                print(self.df[col_list[i]].unique())
+                                input_string = input('Enter mapping list separated by space ')
+                                print("\n")
+                                user_list = input_string.split()
+
+                                # convert each item to int type
+                                for j in range(len(user_list)):
+                                    # convert each item to int type
+                                    map_list.append(int(user_list[j]))
+                                if len(map_list) != len(self.df[col_list[i]].unique()):
+                                    print("len of values or format doesn't seem to match... \n")
+                                else:
+                                    working = True
+
+                            for j in range(len(self.df[col_list[i]].unique())):
+                                map_dic[self.df[col_list[i]].unique()[j]] = map_list[j]
+                            print(map_dic, '\n')
+                            final_check = input('Does the mapping look correct 0 for yes 1 for no: \n')
+                            if final_check == '0':
+                                self.df[col_list[i]] = self.df[col_list[i]].map(map_dic)
+                                print(self.df[col_list[i]])
+                                sleep(1.5)
+                                mapping_bool = True
+                            else:
+                                pass
+                elif index_bool is False:
+                    for col in col:
+                        mapping_bool = False
+                        while mapping_bool == False:
+                            map_dic = {}
+                            working = False
+                            while working == False:
+                                map_list = []
+                                print(self.df[col].unique())
+                                input_string = input('Enter mapping list separated by space ')
+                                print("\n")
+                                user_list = input_string.split()
+
+                                # convert each item to int type
+                                for i in range(len(user_list)):
+                                    # convert each item to int type
+                                    map_list.append(int(user_list[i]))
+                                if len(map_list) != len(self.df[col].unique()):
+                                    print("len of values or format doesn't seem to match... \n")
+                                else:
+                                    working = True
+
+                            for i in range(len(self.df[col].unique())):
+                                map_dic[self.df[col].unique()[i]] = map_list[i]
+                            print(map_dic, '\n')
+                            final_check = input('Does the mapping look correct 0 for yes 1 for no: \n')
+                            if final_check == '0':
+                                self.df[col] = self.df[col].map(map_dic)
+                                print(self.df[col])
+                                sleep(1.5)
+                                mapping_bool = True
+                            else:
+                                pass
+            # one hot ecnconding
+            elif format_type == 3:
+                pass
+            # target
+            if index_bool is True:
+                for i in col:
+                        self.df['Target'] = self.df[col_list[i]]
+                        self.df = self.df.drop(axis=1, columns=[col_list[i]])
+            elif index_bool is False:
+                for col in col:
+                    self.df['Target'] = self.df[col]
+                    self.df = self.df.drop(axis=1, columns=[col])
+            # drop
+            elif format_type == 6:
+                pass
+        print('--------------------------------------------------------------')
 
     # returns different statistics based on entry level
     def df_stats(self, level):
@@ -142,11 +255,12 @@ class Clean:
 
         # level 1 explores the NAN and attempts to show reasonable methods for filling
         if level == 1:
-            if(len(self.df[self.df.isnull().any(axis=1)])) == 0:
+            if (len(self.df[self.df.isnull().any(axis=1)])) == 0:
                 print('Seems to be no NAN...')
             else:
                 print(self.df[self.df.isnull().any(axis=1)])
         print('--------------------------------------------------------------')
+
     # prints data frame
     def print_df(self):
         print(self.df)
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from ml import *
 
 # Example of ml project
 clean = Clean()
-clean.clean_df(r'C:\Users\mxr29\Desktop\ML_TEST\wine.csv', drop_method=0)
-#clean.auto_format(0, auto=False)
+clean.clean_df(r'C:\Users\mxr29\Desktop\New folder\farmed_twitter.csv', drop_method=0)
+clean.format(level = 1, auto=False, format_type=3, col=['Retweets'])
+clean.print_df()
 df = clean.return_df()
 
-ml = Ml()
-ml.tts(df, transform=1, stratify=1, random_state=999)
-ml.model(type='RF')
+# ml = Ml()
+# ml.tts(df, transform=1, stratify=1, random_state=999)
+# ml.model(type='RF')
 
